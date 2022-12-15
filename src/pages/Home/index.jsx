a6fc90c3eddf94b98aec8b068eafecfdd35bb3af
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2022-12-15 18:07:59 +0100

    Ajouter bannière et titre sur la page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,35 @@
 import PictureBanner from '../../assets/home_banner.png';
 import styled from 'styled-components';
+import colors from '../../utils/style/colors';
 
-// L'image d'entête montagne
+// Le conteneur de la bannière image et titre
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
+  position: relative;
   width: 100%;
+  max-width: 1240px;
 `;
 
+// L'image d'entête mer et falaise
 const Banner = styled.img`
   height: 223px;
-  border-radius: 25px;
+  width: 100%;
+  border-radius: 0.5em;
+  object-fit: cover;
+  object-position: 50% 20%;
+  filter: brightness(70%);
+`;
+
+// Le titre
+const Heading = styled.h1`
+  position: absolute;
+  z-index: 100;
+  top: 40%;
+  width: 100%;
+  font-size: 3em;
+  font-weight: 500;
+  color: ${colors.secondary};
+  display: flex;
+  justify-content: center;
 `;
 
 const Section = styled.section`
@@ -26,10 +45,9 @@ function Home() {
     <main>
       <Container>
         <Banner src={PictureBanner} alt="banner" />
+        <Heading>Chez vous, partout et ailleurs</Heading>
       </Container>
-      <Section>
-        <h1>Home</h1>
-      </Section>
+      <Section></Section>
     </main>
   );
 }
