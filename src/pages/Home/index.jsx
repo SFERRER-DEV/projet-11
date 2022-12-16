bbb5f15629238faa9ae2c2e5ca6bb3f1a1dba999
Stef <3736753+SFERRER-DEV@users.noreply.github.com>
2022-12-16 18:29:38 +0100

    Charger les données à partir d'un fichier json depuis le répertoire public/data/

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react';
 import PictureBanner from '../../assets/home_banner.png';
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
+import { Loader } from '../../utils/style/Atoms';
 
 // Le conteneur de la bannière image et titre
 const Container = styled.div`
@@ -37,17 +39,55 @@ const Section = styled.section`
   flex-wrap: wrap;
   justify-content: center;
   width: 100%;
+  background-color: ${colors.tertiary};
   padding: 5em 15em;
+  max-width: 1240px;
+  border-radius: 0.5em;
+  margin: 2em 0;
 `;
 
 function Home() {
+  const [housingData, setHousingData] = useState([]);
+  const [isDataLoading, setDataLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchHousing() {
+      setDataLoading(true);
+      try {
+        const response = await fetch(`/data/logements.json`);
+        const { housingData } = await response.json();
+        setHousingData(housingData);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+      } finally {
+        setDataLoading(false);
+        console.log(housingData);
+      }
+    }
+    fetchHousing();
+  }, []);
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>;
+  }
+
   return (
     <main>
       <Container>
         <Banner src={PictureBanner} alt="banner" />
         <Heading>Chez vous, partout et ailleurs</Heading>
       </Container>
-      <Section></Section>
+      <Section>
+        {isDataLoading ? <Loader /> : <h2>Logements: {housingData.length}</h2>}
+        {housingData.map((housing, index) => (
+          <p style={{ width: '100%' }} key={1000 + index}>
+            <h2>{housing.id}</h2>
+            <h2>{housing.title}</h2>
+          </p>
+        ))}
+      </Section>
     </main>
   );
 }
--- src/utils/style/Atoms.jsx
+++ src/utils/style/Atoms.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import colors from './colors';
+import styled, { keyframes } from 'styled-components';
 
 export const StyledLink = styled(Link)`
   color: ${colors.primary};
@@ -9,4 +9,24 @@ export const StyledLink = styled(Link)`
   margin: 0 32px;
   text-align: center;
   cursor: pointer;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loader = styled.div`
+  padding: 10px;
+  border: 6px solid ${colors.primary};
+  border-bottom-color: transparent;
+  border-radius: 22px;
+  animation: ${rotate} 1s infinite linear;
+  height: 0;
+  width: 0;
 `;
